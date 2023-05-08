684302d01f57a878c144461e5cab65467922ed2d
aditya0genius <adityadeshmukh168@gmail.com>
2023-05-08 14:29:49 +0530

    added services

--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Card from './Card';
 
 function OurServices() {
   return (
-    <div className='container'>
+    <div className='container-fluid col-md-12 '>
         
         <h2>We Provides Venues For </h2>
         <hr />
-        <ul>
-            <li><h3>Marriages</h3></li>
-            <li><h3>Birthday Parties</h3></li>
-            <li> <h3>Engagements</h3></li>
-            <li><h3>Pool Parties</h3></li>
-        </ul>
+            <div className="flex-container">
+            <Card
+                src="/wedding-venue.jpg"
+                title="Book Wedding Venues"
+                description="Make your wedding dream come true explore spacious venues around your town."
+            />
+            <Card
+                src="/decorators.jpg"
+                title="Book for Engagement"
+                description="Make your moments beautiful with the magnificient decorations."
+            />
+            <Card
+                src="/birthdayParty_home.jpg"
+                title="Birthday Parties"
+                description="Clebrate your special day and leave everything to us."
+            />
+            <Card
+                src="/poolParty_home.jpg"
+                title="Pool Party"
+                description="Book your venue with the splashes of water to mistify your special day"
+            />
+            <Card
+                src="\collegeParty_home.jpg"
+                title="College Party"
+                description="Get the better deals at your nearby Venues for College Freshers/Farewell Party"
+            />
+            <Card
+                src="\KittyParty_home.jpg"
+                title="Kitty Party"
+                description="Get venues for small gathering starting from 25 people"
+            />
+            </div>
+            
         
         
        
